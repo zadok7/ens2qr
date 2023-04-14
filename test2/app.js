f9417b263a425ddee02a400b13401566fe7b4898
zadok7 <88821511+zadok7@users.noreply.github.com>
2023-04-14 07:25:01 -0400

    resolve records and generate QRs

---
+++ test2/app.js
@@ -0,0 +1,202 @@
+const ENS_REGISTRY_ADDRESS = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
+const form = document.getElementById('ens-form');
+const ensNameInput = document.getElementById('ens-name');
+const resultsOutput = document.getElementById('results');
+const resolveButton = document.getElementById('resolve');
+
+
+const ENS_RESOLVER_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "addr",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "string",
+        "name": "key",
+        "type": "string"
+      }
+    ],
+    "name": "text",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+const ENS_REGISTRY_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "resolver",
+    "outputs": [
+      {
+        "internalType": "contract IENSResolver",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+
+async function getRecord(ensName, key) {
+  try {
+    const provider = new ethers.providers.getDefaultProvider('mainnet');
+    const ensRegistryContract = new ethers.Contract(ENS_REGISTRY_ADDRESS, ENS_REGISTRY_ABI, provider);
+    const namehash = ethers.utils.namehash(ensName);
+    const resolverAddress = await ensRegistryContract.resolver(namehash);
+    const ensResolverContract = new ethers.Contract(resolverAddress, ENS_RESOLVER_ABI, provider);
+    return await ensResolverContract.text(namehash, key);
+  } catch (error) {
+    console.error(`Error fetching ${key} record:`, error);
+    return '';
+  }
+}
+
+async function getAddress(ensName) {
+  try {
+    const provider = new ethers.providers.getDefaultProvider('mainnet');
+    const ensRegistryContract = new ethers.Contract(ENS_REGISTRY_ADDRESS, ENS_REGISTRY_ABI, provider);
+    const namehash = ethers.utils.namehash(ensName);
+    const resolverAddress = await ensRegistryContract.resolver(namehash);
+    const ensResolverContract = new ethers.Contract(resolverAddress, ENS_RESOLVER_ABI, provider);
+    const ethAddress = await ensResolverContract.addr(namehash);
+    return ethAddress;
+  } catch (error) {
+    console.error('Error fetching address:', error);
+    return '';
+  }
+}
+
+async function getTwitter(ensName) {
+  return getRecord(ensName, 'com.twitter');
+}
+
+async function getTelegram(ensName) {
+  return getRecord(ensName, 'org.telegram');
+}
+
+async function getGithub(ensName) {
+  return getRecord(ensName, 'com.github');
+}
+
+async function getEmail(ensName) {
+  return getRecord(ensName, 'email');
+}
+
+async function getURL(ensName) {
+  return getRecord(ensName, 'url');
+}
+
+async function resolveRecords(ensName) {
+  const [ethAddress, twitterName, telegramName, githubName, emailAddress, url] = await Promise.all([
+    getAddress(ensName),
+    getTwitter(ensName),
+    getTelegram(ensName),
+    getGithub(ensName),
+    getEmail(ensName),
+    getURL(ensName),
+  ]);
+
+  return {
+    ethAddress: ethAddress || 'Address not found',
+    twitterName: twitterName || 'Twitter name not found',
+    telegramName: telegramName || 'Telegram name not found',
+    githubName: githubName || 'GitHub name not found',
+    emailAddress: emailAddress || 'Email not found',
+    url: url || 'URL not found',
+  };
+}
+
+/*
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const ensName = ensNameInput.value;
+  const results = await resolveRecords(ensName);
+  const output = {
+    ensName,
+    ...results,
+  };
+  resultsOutput.textContent = JSON.stringify(output, null, 2);
+});
+*/
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const ensName = ensNameInput.value;
+  const results = await resolveRecords(ensName);
+  const output = {
+    ensName,
+    ...results,
+  };
+  generateQRCodeForEachKeyValuePair(output);
+});
+
+function generateQRCodeForEachKeyValuePair(data) {
+  const container = document.getElementById('qr-codes');
+  container.innerHTML = ''; // Clear the container
+
+  for (const key in data) {
+    const value = data[key];
+
+    // Create a wrapper for each QR code
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'inline-block';
+    wrapper.style.margin = '10px';
+
+    const title = document.createElement('h3');
+    title.textContent = key;
+    wrapper.appendChild(title);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    wrapper.appendChild(canvas);
+    container.appendChild(wrapper);
+
+    QRCode.toCanvas(canvas, value, {
+      width: canvas.width,
+      height: canvas.height,
+      errorCorrectionLevel: 'H' // Set error correction level to 'H'
+    }, (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+}
